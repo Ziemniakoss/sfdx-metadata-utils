667788583074b4b8998d15ba4fb254162b4433fd
Przemek <rawa.przemek@gmail.com>
2021-12-19 00:33:06 +0100

    formatted code with prettier

--- src/commands/metadata-utils/labels/verify.ts
+++ src/commands/metadata-utils/labels/verify.ts
@@ -1,11 +1,12 @@
-import {SfdxCommand, flags} from "@salesforce/command";
-import {AnyJson} from "@salesforce/ts-types";
-import {XmlUtils} from "../../../utils/XmlUtils";
-import {LabelsFilesFinder} from "../../../metadata-files-finders/LabelsFilesFinder";
-import {SfdxError} from "@salesforce/core";
+import { SfdxCommand, flags } from "@salesforce/command";
+import { AnyJson } from "@salesforce/ts-types";
+import { XmlUtils } from "../../../utils/XmlUtils";
+import { LabelsFilesFinder } from "../../../metadata-files-finders/LabelsFilesFinder";
+import { SfdxError } from "@salesforce/core";
 
 export default class VerifyLabels extends SfdxCommand {
-	public static description = "Check if custom labels file does not contain duplicates";
+	public static description =
+		"Check if custom labels file does not contain duplicates";
 
 	private xmlUtils = new XmlUtils();
 
@@ -16,45 +17,56 @@ export default class VerifyLabels extends SfdxCommand {
 	async verifyAllFiles(): Promise<AnyJson> {
 		const files = await new LabelsFilesFinder().findFiles();
 		const labelsOccurrencesMap = new Map<string, LabelOccurrence>();
-		for(const fileName of files) {
-			const dirtyLabelsXml = await this.xmlUtils.readXmlFromFile(fileName)
-				.then(xmlString => this.xmlUtils.convertXmlStringToJson(xmlString))
-			for(const label of dirtyLabelsXml.CustomLabels.labels ?? []) {
+		for (const fileName of files) {
+			const dirtyLabelsXml = await this.xmlUtils
+				.readXmlFromFile(fileName)
+				.then((xmlString) =>
+					this.xmlUtils.convertXmlStringToJson(xmlString)
+				);
+			for (const label of dirtyLabelsXml.CustomLabels.labels ?? []) {
 				const labelFullName = label.fullName[0];
 
 				let labelOccurrences = labelsOccurrencesMap.get(labelFullName);
-				if(labelOccurrences == null) {
+				if (labelOccurrences == null) {
 					labelOccurrences = new LabelOccurrence(labelFullName);
 					labelsOccurrencesMap.set(labelFullName, labelOccurrences);
 				}
-				labelOccurrences.addOccurrence(fileName)
+				labelOccurrences.addOccurrence(fileName);
 			}
 		}
 		const errors = [];
-		for(const labelOccurrence of labelsOccurrencesMap.values()) {
-			if(labelOccurrence.totalOccurrences > 1) {
-				for(const fileWithLabel of labelOccurrence.filesWithThisLabel) {
-					const message = `Duplicates of label ${labelOccurrence.fullName} found in files: ${[...labelOccurrence.filesWithThisLabel.values()].join(",")}`
+		for (const labelOccurrence of labelsOccurrencesMap.values()) {
+			if (labelOccurrence.totalOccurrences > 1) {
+				for (const fileWithLabel of labelOccurrence.filesWithThisLabel) {
+					const message = `Duplicates of label ${
+						labelOccurrence.fullName
+					} found in files: ${[
+						...labelOccurrence.filesWithThisLabel.values(),
+					].join(",")}`;
 					errors.push({
 						fileName: fileWithLabel,
 						label: labelOccurrence.fullName,
-						message
-					})
-					this.ux.error(message)
+						message,
+					});
+					this.ux.error(message);
 				}
 			}
 		}
-		if(errors.length > 0) {
+		if (errors.length > 0) {
 			//TODO better error handling
-			throw new SfdxError("Validation failed", "VALIDATION_FAILED", errors)
+			throw new SfdxError(
+				"Validation failed",
+				"VALIDATION_FAILED",
+				errors
+			);
 		} else {
-			return {}
+			return {};
 		}
 	}
 }
 
 class LabelOccurrence {
-	fullName: string
+	fullName: string;
 
 	_totalOccurrences = 0;
 
